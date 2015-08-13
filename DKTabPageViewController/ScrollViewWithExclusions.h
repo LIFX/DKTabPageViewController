579e5932002bf9b75cb45998857b258a79f0e4c8
Aaron Vernon <gloroth@gmail.com>
2015-08-13 13:44:29 +1000

    Added ScrollViewWithExclusions and made use of it in DKTabPageViewController so that if the content view controller responds to exclusionViews then they will be added to the scroll view for exclusion when the content view is the active one. Could be made more explicit by adding a protocol that content view controllers conform to. This change allows subviews of the scroll view to be excluded completely from scrolling interaction.

---
+++ DKTabPageViewController/ScrollViewWithExclusions.h
@@ -0,0 +1,11 @@
+//
+//  Copyright (c) 2014 LIFX Labs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ScrollViewWithExclusions : UIScrollView
+
+@property (nonatomic) NSArray *exclusionViews;
+
+@end
